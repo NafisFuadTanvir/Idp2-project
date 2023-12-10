55c9ed36bb148ecb0a8caefc2a74f523c34b2600
NafisFuadTanvir <nafisf026@gmail.com>
2023-12-11 00:16:26 +0600

    full backend code finished also merge it with mongodb database

--- Housing Marketplace/Server/index.js
+++ Housing Marketplace/Server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import { userRoute } from './routes/residencyRoute';
+import { residencyRoute } from './routes/userRoute';
 dotenv.config();
 
 const app= express();
@@ -15,3 +17,5 @@ app.listen(PORT,()=>{
     console.log(`server runs at port ${PORT}`)
 })
 
+app.use('/api/user', userRoute)
+app.use("/api/residency", residencyRoute)
