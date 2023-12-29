99999d6c5bd5871900b50783f6bf4ba8c67943e2
NafisFuadTanvir <nafisf026@gmail.com>
2023-12-30 01:36:27 +0600

    adding full client with server and trying to fixing the error

--- Housing Marketplace/Server/index.js
+++ Housing Marketplace/Server/index.js
@@ -2,20 +2,21 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import { userRoute } from './routes/residencyRoute';
-import { residencyRoute } from './routes/userRoute';
-dotenv.config();
+import { userRoute } from './routes/userRoute.js';
+import { residencyRoute } from './routes/residencyRoute.js';
+dotenv.config()
 
-const app= express();
-const PORT= process.env.PORT || 8000;
+const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
 
-app.listen(PORT,()=>{
-    console.log(`server runs at port ${PORT}`)
-})
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 app.use('/api/user', userRoute)
 app.use("/api/residency", residencyRoute)
